22c886cba3ab2d3afe195f938268d59c74e20707
shanreed25 <shannontlreed@gmail.com>
2026-09-17 15:09:58 -0700

    task 2 complete

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function formatFullName(firstName, lastName) {
     }
 
 }
+
+let userName = formatFullName("shannon", "reed")
+console.log(userName);//Reed, Shannon
+//===================================================================================================
+
+
+// Task 2 & 4: Mathematical Operations with Multiple Parameters
+function calculateTotalCost(price, quantity, taxRate, discount=0) {//include an optional discount parameter
+
+    //ensure that price, quantity, and taxRate are all valid numbers
+    if (typeof price !== "number" || typeof quantity  !== "number" || typeof taxRate !== "number"){
+        return "Invalid Input"
+    } 
+    
+    totalCost = (price * quantity) * (1 + taxRate)//calculate the total cost of items, including tax
+
+    if(discount !== 0){//If the discount is provided, the function should subtract the discount from the total cost before applying tax
+        discountAmount = totalCost * (discount / 100)
+        finalCost = totalCost - discountAmount
+        return finalCost 
+
+    } else {
+        return totalCost//If no discount is provided, calculate the total cost as before
+    }     
+
+}
+
+cost = calculateTotalCost(45, 1, 0, 10)
+
+console.log(cost);//40.5
+
